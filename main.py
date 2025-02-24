5144740f43dc949ec1533dff9e8756f23c7bfda6
rikka <katelynuhler470@gmail.com>
2025-02-24 08:02:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -156,11 +156,10 @@ class SetuPlugin(Star):
 
 
     @llm_tool(name="search_setu")
-    async def search_setu_tool(self, event: AstrMessageEvent, num: int, tags: str):
+    async def search_setu_tool(self, event: AstrMessageEvent, num: int):
         '''根据用户希望发送涩图,当用户要求或者希望你给他1份涩图或者1张涩图时调用此工具
         Args:
             num(number): 请求数量
-            tags(string): 标签 tags
         '''  
         nums = int(num)
         try:
